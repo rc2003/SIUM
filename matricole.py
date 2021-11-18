d75972072d879522d2005140a1853553ede9f112
Riccardo Consiglio <47518595+rc2003@users.noreply.github.com>
2021-11-18 08:44:07 +0100

    Update matricole.py

--- matricole.py
+++ matricole.py
@@ -1,5 +1,6 @@
 max_record = 10
 num = int(input("Inserisci il numero di matricole: "))
+print("------------------------------------------")
 
 if num > max_record:
 	print("Superato numero massimo di matricole possibili")
@@ -14,4 +15,5 @@ for persona in range(num):
 	matricola["IDMatricola"] = input("Inserisci il numero di matricola: ")
 	matricola["Cognome"] = input("Inserisci il cognome della matricola: ")
 	matricola["Nome"] = input("Inserisci il nome della matricola: ")
+	print("------------------------------------------")
 	file.write("\n"+matricola["IDMatricola"]+", "+matricola["Cognome"]+", "+matricola["Nome"])
